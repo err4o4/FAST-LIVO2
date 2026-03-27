26c39dd0da9d923d4f3f6548476b93e429a60c5a
Ivan Savelyev <err4o4@MacBook-Pro-alexbrazhnik.local>
2026-03-27 15:43:34 +0100

    fix pcd saving

--- src/LIVMapper.cpp
+++ src/LIVMapper.cpp
@@ -1176,19 +1176,23 @@ void LIVMapper::publish_frame_world(const ros::Publisher &pubLaserCloudFullRes, 
 
   /*** Publish Frame ***/
   sensor_msgs::PointCloud2 laserCloudmsg;
-  if (slam_mode_ == LIVO && LidarMeasures.lio_vio_flg == VIO)
+  bool do_publish = false;
+  if (slam_mode_ == LIVO && LidarMeasures.lio_vio_flg == VIO && !laserCloudWorldRGB->empty())
   {
-    if (laserCloudWorldRGB->empty()) return;
     pcl::toROSMsg(*laserCloudWorldRGB, laserCloudmsg);
+    do_publish = true;
   }
   else if (slam_mode_ == ONLY_LIO || slam_mode_ == ONLY_LO)
   {
     pcl::toROSMsg(*pcl_w_wait_pub, laserCloudmsg);
+    do_publish = true;
   }
-  else return;
-  laserCloudmsg.header.stamp = ros::Time::now(); //.fromSec(last_timestamp_lidar);
-  laserCloudmsg.header.frame_id = "camera_init";
-  pubLaserCloudFullRes.publish(laserCloudmsg);
+  if (do_publish)
+  {
+    laserCloudmsg.header.stamp = ros::Time::now(); //.fromSec(last_timestamp_lidar);
+    laserCloudmsg.header.frame_id = "camera_init";
+    pubLaserCloudFullRes.publish(laserCloudmsg);
+  }
 
   /**************** save map ****************/
   /* 1. make sure you have enough memories
